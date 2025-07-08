44f3722a6b63e8b4760cda91703765696c148a10
Khaledc <kncharkie@gmail.com>
2025-07-08 16:53:08 +0300

    Add project skeleton + theme

---
+++ src/screens/BlogDetails.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const BlogDetails = (props: Props) => {
+  return (
+    <View>
+      <Text>BlogDetails</Text>
+    </View>
+  );
+};
+
+export default BlogDetails;
+
+const styles = StyleSheet.create({});
---
+++ src/theme/theme.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export const colors = {
+  primary: "#FF6FA1",
+  secondary: "#6C63FF",
+  background: "#FDFBFF",
+  card: "#FFFFFF",
+  textDark: "#333333",
+  textLight: "#777777",
+  accent: "#FFB74D",
+};
+export const fontSizes = {
+  sm: 14,
+  md: 18,
+  md1: 20,
+  md2: 22,
+  bg: 26,
+};
+
+export const mainStyles = StyleSheet.create({
+  marginH: {
+    marginHorizontal: 20,
+  },
+  marginV: {
+    marginVertical: 10,
+  },
+});
